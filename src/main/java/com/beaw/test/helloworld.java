fe4abf91390d9e80d58b5af383fb99140dc127d7
Ian <ian.yoo@kaonsoft.com>
2024-03-25 21:57:23 +0900

    변경

--- src/main/java/com/beaw/test/helloworld.java
+++ src/main/java/com/beaw/test/helloworld.java
@@ -3,5 +3,6 @@ package com.beaw.test;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello dolbom");
     }
 }
